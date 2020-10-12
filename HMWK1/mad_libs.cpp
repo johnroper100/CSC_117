1f8fad35bc0cbec5924ee643c1aaa8ca109dc5a7
John Roper <johnroper100@gmail.com>
2020-10-12 02:11:10 -0400

    add base files

---
+++ HMWK1/mad_libs.cpp
@@ -0,0 +1,53 @@
+//File Name: mad_libs.cpp
+//Author: John Roper
+//Email Address: john.roper@assumption.edu
+//Assignment Number: 1
+//Description: A computerized grame of mad libs.
+//Last Changed: 8/12/20, see https://github.com/johnroper100/CSC_117 for full changes
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string instructorName, playerName, food, number, adjective, color, animal;
+
+    cout << "MAD LIBS!\nWelcome, what's your name? ";
+
+    getline(cin, playerName); // Using getline here so as to support multiple-word responses
+
+    cout << "What's your professor's name? ";
+
+    getline(cin, instructorName);
+
+    cout << "Do you have a favorite food? Write it here: ";
+
+    getline(cin, food);
+
+    cout << "Pick a number in the range of 100-120: ";
+
+    getline(cin, number);
+
+    cout << "My favorite adjective is fantabulous, whats yours? Write it here: ";
+
+    getline(cin, adjective);
+
+    cout << "Roses are red, violets are blue, I know you'll tell me your favorite color, oh yes I do! What is it? ";
+
+    getline(cin, color);
+
+    cout << "Whats your favorite animal? ";
+
+    getline(cin, animal);
+
+    cout << "\nDear Prof. " << instructorName << ",\n\n";
+    cout << "I am sorry that I am unable to turn in my homework at this time. First, I ate a rotten " << food << ", which made me turn "
+         << color << " and extremely ill. I came down with a fever of " << number << "."
+         << "Next, my " << adjective << " pet " << animal
+         << " must have smelled the remains of the " << food << " on my homework, because he ate it. I am currently rewriting my homework and hope you will accept it late.\n\n";
+    cout << "Sincerely,\n"
+         << playerName << endl;
+    return 0;
+}
---
+++ base.cpp
@@ -0,0 +1,14 @@
+//File Name: base.cpp
+//Author: John Roper
+//Email Address: john.roper@assumption.edu
+//Assignment Number: 1
+//Description: Sample program
+//Last Changed: 8/12/20, see https://github.com/johnroper100/CSC_117 for full changes
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
