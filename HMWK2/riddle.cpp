a831e199596437211931c0045d9931a7e4624144
John Roper <johnroper100@gmail.com>
2020-10-23 13:31:40 -0400

    add riddle

--- HMWK2/riddle.cpp
+++ HMWK2/riddle.cpp
@@ -9,11 +9,32 @@
 
 using namespace std;
 
-int main() {
-    int d1;
-    int d2;
-    int d3;
-    int d4;
-    cout << "The address is " << endl;
+int main()
+{
+    for (int ones = 0; ones < 10; ones++)
+    {
+        for (int tens = 0; tens < 10; tens++)
+        {
+            for (int hundreds = 0; hundreds < 10; hundreds++)
+            {
+                for (int thousands = 0; thousands < 10; thousands++)
+                {
+                    if ((thousands != hundreds && thousands != tens && thousands != ones) && (hundreds != tens && hundreds != ones) && (tens != ones))
+                    {
+                        if ((tens * 3) == thousands)
+                        {
+                            if (ones % 2 != 0)
+                            {
+                                if (ones + tens + hundreds + thousands == 27)
+                                {
+                                    cout << "The address is " << thousands << hundreds << tens << ones << endl;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
