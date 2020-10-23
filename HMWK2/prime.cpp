51edff5eb0c6e38825c1db250892039ebe8559f3
John Roper <johnroper100@gmail.com>
2020-10-23 12:38:46 -0400

    add prime

---
+++ HMWK2/prime.cpp
@@ -0,0 +1,27 @@
+//File Name: prime.cpp
+//Author: John Roper
+//Email Address: john.roper@assumption.edu
+//Assignment Number: 2
+//Description: Sample program
+//Last Changed: 10/23/20, see https://github.com/johnroper100/CSC_117 for full changes
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "The following numbers in the range 1-100 are prime." << endl;
+    bool isPrime = true;
+    for (int i = 3; i <= 100; i++) {
+        isPrime = true;
+        for (int y = 2; y < i; y++) {
+            if (i % y == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            cout << i << " is a prime number." << endl;
+        }
+    }
+    return 0;
+}
