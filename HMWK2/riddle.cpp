499b58385734b7dec2cafee5a55974295f37757e
John Roper <johnroper100@gmail.com>
2020-10-23 13:08:14 -0400

    start riddle

---
+++ HMWK2/riddle.cpp
@@ -0,0 +1,19 @@
+//File Name: riddle.cpp
+//Author: John Roper
+//Email Address: john.roper@assumption.edu
+//Assignment Number: 2
+//Description: Solve the Riddler's riddle
+//Last Changed: 10/23/20, see https://github.com/johnroper100/CSC_117 for full changes
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int d1;
+    int d2;
+    int d3;
+    int d4;
+    cout << "The address is " << endl;
+    return 0;
+}
