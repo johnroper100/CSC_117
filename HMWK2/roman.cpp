01b5f62870147acff611cccfbd2404aa281007dc
John Roper <johnroper100@gmail.com>
2020-10-23 13:47:39 -0400

    more work on roman

--- HMWK2/roman.cpp
+++ HMWK2/roman.cpp
@@ -10,17 +10,38 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string shouldContiue = "y";
     int arabicValue;
+    int digit;
+    string romanValue;
 
-    while (shouldContiue == "y") {
+    while (shouldContiue == "y")
+    {
         cout << "Please enter a whole value between 1000 and 3000: ";
         cin >> arabicValue;
 
-        
+        if (arabicValue >= 1000 && arabicValue <= 3000)
+        {
+            digit = arabicValue / 1000;
+            romanValue = "";
 
-        cout << "\nThe Roman equivalent of that value is: " << endl;
+            digit = (arabicValue / 100) % 10;
+            romanValue += "";
+
+            digit = (arabicValue / 10) % 10;
+            romanValue += "";
+
+            digit = arabicValue % 10;
+            romanValue += "";
+
+            cout << "\nThe Roman equivalent of that value is: " << romanValue << endl;
+        }
+        else
+        {
+            cout << "\nYou must enter a vlue in the correct range!" << endl;
+        }
 
         cout << "\nWould you like to convert another value (y/n)? ";
         cin >> shouldContiue;
